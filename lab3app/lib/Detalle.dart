d6bbc8a2c372474ac224d3e3ddd3e13ddd7b7d79
Jairo Figueroa <jfigueroadominguez@gmail.com>
2024-05-03 23:24:56 -0400

    edit

--- lab3app/lib/Detalle.dart
+++ lab3app/lib/Detalle.dart
@@ -25,7 +25,6 @@ class Detalle extends StatelessWidget {
                 '',
                 style: TextStyle(fontSize: 24),
               ),
-              const SizedBox(height: 20),
                ElevatedButton(
               onPressed: () {
                   Navigator.push(
--- lab3app/lib/main.dart
+++ lab3app/lib/main.dart
@@ -125,7 +125,7 @@ class _MyHomePageState extends State<MyHomePage> {
           FloatingActionButton(
             onPressed: _incrementCounter,
             tooltip: 'Increment',
-            child: const Icon(Icons.arrow_forward),
+            child: const Icon(Icons.add),
           ),
          
           FloatingActionButton(

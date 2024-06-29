50b7604b4abe4826a3f1436d27dc451486a69a80
Jairo Figueroa <jfigueroadominguez@gmail.com>
2024-06-28 20:39:42 -0400

    update

--- lab3app/lib/Detalle.dart
+++ lab3app/lib/Detalle.dart
@@ -1,41 +1,75 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
 
-class Detalle extends StatelessWidget {
+class Detalle extends StatefulWidget {
   const Detalle({super.key});
 
   @override
+  _DetalleState createState() => _DetalleState();
+}
+
+class _DetalleState extends State<Detalle> {
+  @override
+  void initState() {
+    super.initState();
+    print('Detalle - initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('Detalle - didChangeDependencies');
+  }
+
+  @override
+  void didUpdateWidget(covariant Detalle oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('Detalle - didUpdateWidget');
+  }
+
+  @override
+  void deactivate() {
+    print('Detalle - deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('Detalle - dispose');
+    super.dispose();
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print('Detalle - reassemble');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Detalle',
-      theme: ThemeData(
-        fontFamily: 'NuevaFuente',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    print('Detalle - build');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detalle'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Detalle'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                '',
-                style: TextStyle(fontSize: 24),
-              ),
-               ElevatedButton(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '',
+              style: TextStyle(fontSize: 24),
+            ),
+            ElevatedButton(
               onPressed: () {
-                  Navigator.push(
-               context,
-               MaterialPageRoute(builder: (context) => const MyApp()),
-               );
-               },
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyApp()),
+                );
+              },
               child: const Icon(Icons.arrow_back),
-             ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lab3app/lib/ListaDetalle.dart
+++ lab3app/lib/ListaDetalle.dart
@@ -1,11 +1,53 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
 
-class ListaDetalle extends StatelessWidget {
+class ListaDetalle extends StatefulWidget {
   const ListaDetalle({super.key});
 
   @override
+  _ListaDetalleState createState() => _ListaDetalleState();
+}
+
+class _ListaDetalleState extends State<ListaDetalle> {
+  @override
+  void initState() {
+    super.initState();
+    print('ListaDetalle - initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('ListaDetalle - didChangeDependencies');
+  }
+
+  @override
+  void didUpdateWidget(covariant ListaDetalle oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('ListaDetalle - didUpdateWidget');
+  }
+
+  @override
+  void deactivate() {
+    print('ListaDetalle - deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('ListaDetalle - dispose');
+    super.dispose();
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print('ListaDetalle - reassemble');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('ListaDetalle - build');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista'),
@@ -37,12 +79,12 @@ class ListaDetalle extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-                  Navigator.push(
-               context,
-               MaterialPageRoute(builder: (context) => const MyApp()),
-               );
-               },
-              child: const Icon(Icons.arrow_back),
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const MyApp()),
+          );
+        },
+        child: const Icon(Icons.arrow_back),
       ),
     );
   }

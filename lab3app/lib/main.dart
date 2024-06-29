53ba504961a5770da57015584eb96099007ef36e
Jairo Figueroa <jfigueroadominguez@gmail.com>
2024-06-28 22:48:09 -0400

    change

--- lab3app/lib/main.dart
+++ lab3app/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({superkey, required this.title});
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
